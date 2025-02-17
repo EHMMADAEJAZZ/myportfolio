0dd51464ca68dc754946b02121e1d7c080874fa4
ehmmadaejazz <ehmmadaejazz33@gmail.com>
2025-02-17 18:20:16 +0530

    update production

--- client/src/common/services.js
+++ client/src/common/services.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Axios = axios.create({
-  baseURL: 'http://localhost:5000/api/v1',
+  baseURL: 'https://aijaz-ahmad-portfolio.vercel.app/api/v1',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
